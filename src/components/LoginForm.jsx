90f6951daa3d3b5a63c7b3a323a1faeabb29674a
Rodrigo Henrique <rodhenrc@outlook.com>
2022-07-14 13:19:19 -0300

    Adicionado axios ao projeto e requisições para o server

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,16 +1,46 @@
 import { TextField } from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
 import "../styles/auth.scss";
 
 function LoginForm({ setAuth }) {
+  const [data, setData] = useState({ email: "", password: "" });
+  const url = "http://localhost:8080/auth/login";
+
+  const handleInput = (e) => {
+    setData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const request = await axios.post(url, data);
+
+    console.log(request);
+  };
+
   return (
-    <div className="auth-container">
+    <div className="auth-container" onSubmit={handleLogin}>
       <h2>LOGIN</h2>
       <form className="form">
-        <TextField id="email" type="email" label="E-Mail" variant="filled" />
+        <TextField
+          id="email"
+          label="E-Mail"
+          name="email"
+          onChange={handleInput}
+          type="email"
+          value={data.email}
+          variant="filled"
+        />
         <TextField
           id="password"
-          type="password"
           label="Senha"
+          name="password"
+          onChange={handleInput}
+          type="password"
+          value={data.password}
           variant="filled"
         />
         <button>ENTRAR</button>
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,17 +1,66 @@
 import { TextField } from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
 import "../styles/auth.scss";
 
 function RegisterForm({ setAuth }) {
+  const [msg, setMsg] = useState({ erro: "", sucesso: "" });
+  const [data, setData] = useState({ username: "", email: "", password: "" });
+  const url = "http://localhost:8080/auth/register";
+
+  const handleInput = (e) => {
+    setData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const registerUser = async (e) => {
+    e.preventDefault();
+
+    const request = await axios.post(url, data);
+
+    if (request.status === 500)
+      return setMsg({ erro: request.data.mensagem, sucesso: "" });
+
+    if (request.data.mensagem === "Usuário já cadastrado")
+      return setMsg({ erro: request.data.mensagem, sucesso: "" });
+
+    setMsg({ erro: "", sucesso: "Usuário cadastrado com sucesso!" });
+    setTimeout(setAuth, 3000);
+  };
+
   return (
     <div className="auth-container">
       <h2>REGISTRE-SE</h2>
-      <form className="form">
-        <TextField id="username" type="text" label="Usuário" variant="filled" />
-        <TextField id="email" type="email" label="E-Mail" variant="filled" />
+      {msg.erro}
+      {msg.sucesso}
+      <form className="form" onSubmit={registerUser}>
+        <TextField
+          id="username"
+          label="Usuário"
+          name="username"
+          onChange={handleInput}
+          type="text"
+          value={data.username}
+          variant="filled"
+        />
+        <TextField
+          id="email"
+          label="E-Mail"
+          name="email"
+          onChange={handleInput}
+          type="email"
+          value={data.email}
+          variant="filled"
+        />
         <TextField
           id="password"
-          type="password"
           label="Senha"
+          name="password"
+          onChange={handleInput}
+          type="password"
+          value={data.password}
           variant="filled"
         />
         <button>REGISTRAR</button>
